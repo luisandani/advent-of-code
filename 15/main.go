59703ae6b7e4ca2e5d946f5b44816de0194f8afa
Luis Andani <luisandani@gmail.com>
2021-12-15 17:52:36 +0100

    Solution part 1

--- 15/main.go
+++ 15/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
-	"time"
 )
 
 func main() {
-	grid := loadHeightMap("example")
-	fmt.Printf("%v\n", grid)
+	grid := loadHeightMap("input")
 	walkMap(grid, point{0, 0})
 }
 
@@ -18,71 +17,126 @@ func walkMap(grid [][]int, startPnt point) {
 	gridHeigth := len(grid)
 	gridWidth := len(grid[0])
 	destPoint := point{gridWidth - 1, gridHeigth - 1}
-	fmt.Printf("start: %v | dest: %v\n", startPnt, destPoint)
 
 	visited := make([][]bool, gridHeigth)
 	for i := 0; i < gridHeigth; i++ {
 		visited[i] = make([]bool, gridWidth)
 	}
-	visited[startPnt.row][startPnt.col] = true
 
-	shortestPath := 0
+	vtx := make(map[point]vertex)
+	// we need to create all the possible points
+	for row := 0; row < gridHeigth; row++ {
+		for col := 0; col < gridWidth; col++ {
+			np := point{row, col}
+			vtx[np] = vertex{
+				dist:       math.MaxInt32,
+				prevVertex: nil,
+			}
+		}
+	}
 
-	// applying BFS on matrix cells starting from source
-	var queue []qItem
-	queue = append(queue, qItem{row: startPnt.col, col: startPnt.row, dist: 0})
+	vtx[startPnt] = vertex{0, &startPnt}
+
 	for {
-		currentPoint, queue := queue[len(queue)-1], queue[:len(queue)-1]
-		fmt.Printf("Visited: %v\n", currentPoint)
-		time.Sleep(500 * time.Millisecond)
-		// Destination found;
-		if currentPoint.row == destPoint.row && currentPoint.col == destPoint.col {
-			shortestPath = currentPoint.dist
+		currentPoint, err := getNextVisitablePoint(vtx, visited, startPnt)
+		if err != nil {
 			break
 		}
 
 		// moving up
 		if isValidPoint(currentPoint.row-1, currentPoint.col, grid, visited) {
-			nr := currentPoint.row - 1
-			nc := currentPoint.col
-			nd := currentPoint.dist + grid[nr][nc]
-			queue = append(queue, qItem{nr, nc, nd})
-			visited[nr][nc] = true
+			newPoint := point{currentPoint.row - 1, currentPoint.col}
+			newDistance := vtx[currentPoint].dist + grid[newPoint.row][newPoint.col]
+			if newDistance < vtx[newPoint].dist {
+				nvx := vertex{
+					dist:       newDistance,
+					prevVertex: &currentPoint,
+				}
+				vtx[newPoint] = nvx
+			}
 		}
 
 		// moving down
 		if isValidPoint(currentPoint.row+1, currentPoint.col, grid, visited) {
-			nr := currentPoint.row + 1
-			nc := currentPoint.col
-			nd := currentPoint.dist + grid[nr][nc]
-			queue = append(queue, qItem{nr, nc, nd})
-			visited[nr][nc] = true
+			newPoint := point{currentPoint.row + 1, currentPoint.col}
+			newDistance := vtx[currentPoint].dist + grid[newPoint.row][newPoint.col]
+			if newDistance < vtx[newPoint].dist {
+				nvx := vertex{
+					dist:       newDistance,
+					prevVertex: &currentPoint,
+				}
+				vtx[newPoint] = nvx
+			}
 		}
 
 		// moving left
 		if isValidPoint(currentPoint.row, currentPoint.col-1, grid, visited) {
-			nr := currentPoint.row
-			nc := currentPoint.col - 1
-			nd := currentPoint.dist + grid[nr][nc]
-			queue = append(queue, qItem{nr, nc, nd})
-			visited[nr][nc] = true
+			newPoint := point{currentPoint.row, currentPoint.col - 1}
+			newDistance := vtx[currentPoint].dist + grid[newPoint.row][newPoint.col]
+			if newDistance < vtx[newPoint].dist {
+				nvx := vertex{
+					dist:       newDistance,
+					prevVertex: &currentPoint,
+				}
+				vtx[newPoint] = nvx
+			}
 		}
 
 		// moving right
 		if isValidPoint(currentPoint.row, currentPoint.col+1, grid, visited) {
-			nr := currentPoint.row
-			nc := currentPoint.col + 1
-			nd := currentPoint.dist + grid[nr][nc]
-			queue = append(queue, qItem{nr, nc, nd})
-			visited[nr][nc] = true
+			newPoint := point{currentPoint.row, currentPoint.col + 1}
+			newDistance := vtx[currentPoint].dist + grid[newPoint.row][newPoint.col]
+			if newDistance < vtx[newPoint].dist {
+				nvx := vertex{
+					dist:       newDistance,
+					prevVertex: &currentPoint,
+				}
+				vtx[newPoint] = nvx
+			}
 		}
 
-		if len(queue) < 1 {
-			break
+		visited[currentPoint.row][currentPoint.col] = true
+	}
+
+	fmt.Printf("Shortest Path to last point: %d\n", vtx[destPoint].dist)
+}
+
+func getNextVisitablePoint(vtx map[point]vertex, visited [][]bool, startPoint point) (point, error) {
+	if !visited[startPoint.row][startPoint.col] { // if it's the first iteration we return starting point
+		return startPoint, nil
+	}
+	// from the non-visited vortex
+	// we need to get the smallest distance that previous was the starting point
+	smallestDist := math.MaxInt32
+	res := point{-1, -1}
+	for p, v := range vtx {
+		if p == startPoint || visited[p.row][p.col] { // skip non-pointing to A || already visited
+			continue
+		}
+		if v.dist < smallestDist {
+			smallestDist = v.dist
+			res = p
 		}
 	}
 
-	fmt.Printf("shortest path: %d", shortestPath)
+	if res.col < 0 || res.row < 0 {
+		return res, fmt.Errorf("could not find the next visitable Point")
+	}
+	return res, nil
+}
+
+type vertex struct {
+	dist       int
+	prevVertex *point
+}
+
+func (v vertex) String() string {
+	return fmt.Sprintf("Dist:%d From:%v\n", v.dist, v.prevVertex)
+}
+
+type point struct {
+	row int
+	col int
 }
 
 func isValidPoint(row int, col int, grid [][]int, visited [][]bool) bool {
@@ -92,18 +146,6 @@ func isValidPoint(row int, col int, grid [][]int, visited [][]bool) bool {
 		return true
 	}
 	return false
-}
-
-// qItem for current location and distance from source location
-type qItem struct {
-	row  int
-	col  int
-	dist int
-}
-
-type point struct {
-	row int
-	col int
 }
 
 func loadHeightMap(path string) [][]int {
